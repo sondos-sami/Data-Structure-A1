2f335473dd1486ba76984854a64c77b55c1e17bb
Sondos Sami <143265107+sondos-sami@users.noreply.github.com>
2024-04-23 00:56:41 +0200

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,15 +2,31 @@
 #include <iostream>
 #include <vector>
 #include <string>
-void Student::insertionSort(vector<Student>& students) {
-    int n = students.size();
-    for (int i = 1; i < n; ++i) {
-        Student key = students[i];
-        int j = i - 1;
-        while (j >= 0 && students[j].getName() > key.getName()) {
-            students[j + 1] = students[j];
-            j = j - 1;
+#include <vector>
+using namespace std;
+template <typename T, typename Comparator>
+void InsertionSort(vector<T>& data, Comparator comp) {
+    int n = data.size();
+    for (int i = 1; i < n; i++) {
+        for (int j=0;j<n&& comp(data[i], data[j]);j++){
+            swap(data[i],data[j]);
         }
-        students[j + 1] = key;
+
+    }
+}
+
+template <typename T, typename Comparator>
+void SelectionSort(vector<T>& data, Comparator comp) {
+    int n = data.size();
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (comp(data[j], data[minIndex])) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swap(data[i], data[minIndex]);
+        }
     }
 }
