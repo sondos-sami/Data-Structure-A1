33969730ef35289c7916def4a97c41759ccc807a
nado31 <121806218+nado31@users.noreply.github.com>
2024-04-22 18:50:26 +0200

    Create QueueLinkedBased.cpp

---
+++ QueueLinkedBased.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+using namespace std;
+template<class t>
+class linkedQueue{
+private:
+    struct node{
+        t item;
+        node*next;
+    };
+    node*front,*back;
+    int length;
+public:
+    linkedQueue(){
+        front=back=NULL;
+        length=0;
+    }
+    bool IsEmpty(){
+        return length==0;
+    }
+    int QueueSize(){
+        return length;
+    }
+    void Enqueue( t element){
+        node*newnode=new node;
+        newnode->item=element;
+        newnode->next=NULL;
+        if (length==0){
+          front=back=newnode;
+        }
+        else{
+            back->next=newnode;
+            back=newnode;
+        }
+        length++;
+    }
+    t Dequeue(){
+       if(IsEmpty()){cout<<"can't delete";}
+       if(length==1){
+           t last =front->item;
+          delete front;
+           back=NULL;
+           length=0;
+           return last;
+       }
+       else{
+           node*temp=front;
+           t last =front->item;
+           front=front->next;
+           delete temp;
+            length--;
+           return last;
+       }
+    }
+    t first(){
+        if(!IsEmpty()){
+        return front->item;}
+    }
+    void clear(){
+        if (IsEmpty()) { cout<<"can't delete";}
+        else{
+        node*current = new node;
+        while(front!=NULL){
+            current=front;
+            front=front->next;
+            delete current;
+        }
+        back=NULL;
+        length=0;
+    }}
+    void print(){
+        if (IsEmpty()) { cout<<"can't print";}
+        node*current=front;
+        while(front!=NULL){
+            cout<<current->item<<" ";
+            current = current->next;
+        }
+    }
+};
+
+int main() {
+    linkedQueue<int> q1;
+    q1.Enqueue(20);
+    q1.Enqueue(20);
+    q1.Enqueue(20);
+    q1.Enqueue(20);
+    q1.Enqueue(20);
+    q1.Enqueue(20);
+
+    q1.print();
+
+}
