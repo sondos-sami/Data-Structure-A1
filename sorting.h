b4e99cde00b8fc93d749cba2c256dd327e5ca08d
Sondos Sami <143265107+sondos-sami@users.noreply.github.com>
2024-04-24 19:44:23 +0200

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,13 +1,14 @@
- #include <iostream>
-#include <string>
-#include <vector>
+#ifndef DATASTRUCTURE_SORTING_H
+#define DATASTRUCTURE_SORTING_H
+ #include <vector>
 using namespace std;
 template <typename T, typename Comparator>
 void InsertionSort(vector<T>& data, Comparator comp) {
     int n = data.size();
     for (int i = 1; i < n; i++) {
-        for (int j=0;j<n&& comp(data[i], data[j]);j++){
-            swap(data[i],data[j]);
+        for (int j=0;j<i;j++){
+            if(comp(data[i],data[j]))
+                 swap(data[i],data[j]);
         }
 
     }
@@ -28,24 +29,6 @@ void SelectionSort(vector<T>& data, Comparator comp) {
         }
     }
 }
+ 
 
-
-// Function Bubble Sort
-template <typename T, typename Comparator>
-void BubbleSort(vector<T>& data, Comparator comp) {
-    bool f;
-    int n = data.size();
-    for (int i = 0; i < n - 1; ++i) {
-        f = false;
-        for (int j = n - 1; j > i; j--) {
-            if (comp(data[j], data[j - 1])) //based on the comparator byName or byGPA and < or >
-            {
-                swap(data[j], data[j - 1]);
-                f = true;
-            }
-        }
-        if (!f)//to achieve best case o(n)
-            return;
-    }
-}
-
+#endif //DATASTRUCTURE_SORTING_H
