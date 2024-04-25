7739f94c3a226a44c937d3d174c597c265c8f3ec
mariameid210 <147279883+mariameid210@users.noreply.github.com>
2024-04-25 15:35:51 +0200

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,25 +1,33 @@
 #ifndef DATASTRUCTURE_SORTING_H
 #define DATASTRUCTURE_SORTING_H
- #include <vector>
+
+#include <vector>
+#include <chrono>
+
 using namespace std;
+using namespace chrono;
+
 template <typename T, typename Comparator>
-void InsertionSort(vector<T>& data, Comparator comp) {
+void InsertionSort(vector<T>& data, Comparator comp, int& comparisons) {
+    comparisons = 0;
     int n = data.size();
     for (int i = 1; i < n; i++) {
-        for (int j=0;j<i;j++){
-            if(comp(data[i],data[j]))
-                 swap(data[i],data[j]);
+        for (int j = 0; j < i; j++) {
+            comparisons++;
+            if (comp(data[i], data[j]))
+                swap(data[i], data[j]);
         }
-
     }
 }
 
 template <typename T, typename Comparator>
-void SelectionSort(vector<T>& data, Comparator comp) {
+void SelectionSort(vector<T>& data, Comparator comp, int& comparisons) {
+    comparisons = 0;
     int n = data.size();
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
+            comparisons++;
             if (comp(data[j], data[minIndex])) {
                 minIndex = j;
             }
@@ -29,34 +37,37 @@ void SelectionSort(vector<T>& data, Comparator comp) {
         }
     }
 }
-//Shell Sort
+
 template <typename T, typename Comparator>
-void ShellSort(vector<T>& data, Comparator comp) {
+void ShellSort(vector<T>& data, Comparator comp, int& comparisons) {
+    comparisons = 0;
     int n = data.size();
     for (int gap = n / 2; gap > 0; gap /= 2) {
         for (int i = gap; i < n; i++) {
             for (int j = i; j >= gap && comp(data[j], data[j - gap]); j -= gap) {
+                comparisons++;
                 swap(data[j], data[j - gap]);
             }
         }
     }
 }
-//Bubble Sort
+
 template <typename T, typename Comparator>
-void BubbleSort(vector<T>& data, Comparator comp) {
+void BubbleSort(vector<T>& data, Comparator comp, int& comparisons) {
+    comparisons = 0;
     bool f;
     int n = data.size();
     for (int i = 0; i < n - 1; ++i) {
         f = false;
-        for (int j = n-1; j >i; j--) {
-            if (comp(data[j],data[j-1]))//based on the comparator byName or byGPA and < or >
-            {
-                swap(data[j],data[j-1]);
+        for (int j = n - 1; j > i; j--) {
+            comparisons++;
+            if (comp(data[j], data[j - 1])) {
+                swap(data[j], data[j - 1]);
                 f = true;
             }
         }
-        if (!f)//to achieve best case o(n)
-         return;
+        if (!f) // to achieve best case O(n)
+            return;
     }
 }
 
